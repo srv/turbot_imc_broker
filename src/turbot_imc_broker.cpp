977a5c77a763f6cefd5e2130a002eb61dcfbe2ad
Miquel Massot <miquel.massot@uib.cat>
2017-11-04 11:05:25 +0100

    Working example of NavSts to EstimatedState

--- src/turbot_imc_broker.cpp
+++ src/turbot_imc_broker.cpp
@@ -21,18 +21,22 @@
 //  DEALINGS IN THE SOFTWARE.
 
 #include <turbot_imc_broker/turbot_imc_broker.h>
+
+#include <ros_imc_broker/ImcTypes.hpp>
+#include <IMC/Base/Packet.hpp>
+#include <IMC/Spec/AllMessages.hpp>
 #include <IMC/Spec/EstimatedState.hpp>
 
 TurbotIMCBroker::TurbotIMCBroker() {
   ros::NodeHandle nhp("~");
-  estimated_state_pub_ = nhp.advertise<IMC::EstimatedState>("estimated_state", 1);
-  nav_sts_sub_ = nhp.subscribe("nav_sts", 1, &TurbotIMCBroker::NavStsCallback, this);
+  estimated_state_pub_ = nhp.advertise<IMC::EstimatedState>("/IMC/In/EstimatedState", 1);
+  nav_sts_sub_ = nhp.subscribe("/navigation/nav_sts", 1, &TurbotIMCBroker::NavStsCallback, this);
 }
 
 void TurbotIMCBroker::NavStsCallback(const auv_msgs::NavStsConstPtr& msg) {
   IMC::EstimatedState imc_msg;
-  imc_msg.lat = msg->global_position.latitude;
-  imc_msg.lon = msg->global_position.longitude;
+  imc_msg.lat = msg->global_position.latitude*M_PI/180.0;
+  imc_msg.lon = msg->global_position.longitude*M_PI/180.0;
   imc_msg.height = -msg->position.depth;
   imc_msg.x = msg->position.north;
   imc_msg.y = msg->position.east;
@@ -40,9 +44,9 @@ void TurbotIMCBroker::NavStsCallback(const auv_msgs::NavStsConstPtr& msg) {
   imc_msg.phi = msg->orientation.roll;
   imc_msg.theta = msg->orientation.pitch;
   imc_msg.psi = msg->orientation.yaw;
-  imc_msg.u = msg->body_velocity.x;
-  imc_msg.v = msg->body_velocity.y;
-  imc_msg.w = msg->body_velocity.z;
+  imc_msg.u = msg->body_velocity.x;  // is it correct?
+  imc_msg.v = msg->body_velocity.y;  // is it correct?
+  imc_msg.w = msg->body_velocity.z;  // is it correct?
   imc_msg.vx = msg->body_velocity.x;
   imc_msg.vy = msg->body_velocity.y;
   imc_msg.vz = msg->body_velocity.z;
--- src/turbot_imc_broker_node.cpp
+++ src/turbot_imc_broker_node.cpp
@@ -23,8 +23,9 @@
 #include <turbot_imc_broker/turbot_imc_broker.h>
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "turbot_imc_broker");
-    TurbotIMCBroker tb;
-    ros::spin();
-    return 0;
+  ros::init(argc, argv, "turbot_imc_broker");
+  TurbotIMCBroker tb;
+  ros::MultiThreadedSpinner spinner(4);  // Use 4 threads
+  spinner.spin();  // spin() will not return until the node has been shutdown
+  return 0;
 }
