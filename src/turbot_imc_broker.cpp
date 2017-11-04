c63bed9adba70971ee7b5bb88481a1646a782374
Miquel Massot <miquel.massot@uib.cat>
2017-11-04 14:25:11 +0100

    Fix ground velocity fields

--- src/turbot_imc_broker.cpp
+++ src/turbot_imc_broker.cpp
@@ -22,9 +22,14 @@
 
 #include <turbot_imc_broker/turbot_imc_broker.h>
 
+#include <tf/tf.h>
+
+// Base IMC template
 #include <ros_imc_broker/ImcTypes.hpp>
 #include <IMC/Base/Packet.hpp>
 #include <IMC/Spec/AllMessages.hpp>
+
+// IMC messages to use: classes will be IMC::MessageType
 #include <IMC/Spec/EstimatedState.hpp>
 
 TurbotIMCBroker::TurbotIMCBroker() {
@@ -35,25 +40,45 @@ TurbotIMCBroker::TurbotIMCBroker() {
 
 void TurbotIMCBroker::NavStsCallback(const auv_msgs::NavStsConstPtr& msg) {
   IMC::EstimatedState imc_msg;
-  imc_msg.lat = msg->global_position.latitude*M_PI/180.0;
-  imc_msg.lon = msg->global_position.longitude*M_PI/180.0;
-  imc_msg.height = -msg->position.depth;
+  // NED origin
+  imc_msg.lat = msg->origin.latitude*M_PI/180.0;
+  imc_msg.lon = msg->origin.longitude*M_PI/180.0;
+  imc_msg.height = 0;
+
+  // Offset WRT NED origin
   imc_msg.x = msg->position.north;
   imc_msg.y = msg->position.east;
   imc_msg.z = msg->position.depth;
+  imc_msg.depth = msg->position.depth;
+  imc_msg.alt = msg->altitude;
+
+  // Vehicle orientation
   imc_msg.phi = msg->orientation.roll;
   imc_msg.theta = msg->orientation.pitch;
   imc_msg.psi = msg->orientation.yaw;
-  imc_msg.u = msg->body_velocity.x;  // is it correct?
-  imc_msg.v = msg->body_velocity.y;  // is it correct?
-  imc_msg.w = msg->body_velocity.z;  // is it correct?
-  imc_msg.vx = msg->body_velocity.x;
-  imc_msg.vy = msg->body_velocity.y;
-  imc_msg.vz = msg->body_velocity.z;
+
+  // Body-fixed frame speeds
+  imc_msg.u = msg->body_velocity.x;
+  imc_msg.v = msg->body_velocity.y;
+  imc_msg.w = msg->body_velocity.z;
+
+  // Body-fixed angular velocity
   imc_msg.p = msg->orientation_rate.roll;
   imc_msg.q = msg->orientation_rate.pitch;
   imc_msg.r = msg->orientation_rate.yaw;
-  imc_msg.depth = msg->position.depth;
-  imc_msg.alt = msg->altitude;
+
+  // Transform velocities to NED frame
+  tf::Matrix3x3 R;
+  R.setRPY(msg->orientation.roll,
+           msg->orientation.pitch,
+           msg->orientation.yaw);
+  tf::Vector3 body_velocity(msg->body_velocity.x, msg->body_velocity.y, msg->body_velocity.z);
+  tf::Vector3 ned_velocity = R.inverse()*body_velocity;
+
+  // NED frame speeds
+  imc_msg.vx = ned_velocity.x();
+  imc_msg.vy = ned_velocity.y();
+  imc_msg.vz = ned_velocity.z();
+
   estimated_state_pub_.publish(imc_msg);
 }
